f73216ccea4cfa853d3663544c619f61d9d0d9ae
linmalin <linmalin@yahoo.com>
2013-09-23 14:29:44 -0700

    Expanded logo hover area, adjusted colour, layout and type

---
+++ assets/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Master Template. This template will be used to display your blog posts and pages if page.php does not exist.
+ *
+ * @package blm_basic
+ */
+
+get_header(); ?>
+<h1> Child theme over rides!</h1>
+
+<div id="main">
+	
+	<section id="content">
+		
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<p class="post-date"><?php echo get_the_date(); ?></p>
+			<div class="post-line"></div>
+			<?php the_content( __( 'Read more', 'blm_basic' ) ); ?>
+				
+			<?php get_template_part( 'inc/meta' ); ?>
+
+		</article>
+		
+	<?php endwhile; endif; ?>
+	
+	<?php get_template_part( 'inc/nav' ); ?>
+	
+	</section><!-- #content -->
+
+<?php get_sidebar(); ?>
+
+</div><!-- #main -->
+
+<?php get_footer(); ?>
